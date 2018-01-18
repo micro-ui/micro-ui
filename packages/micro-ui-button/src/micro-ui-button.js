3790e1b352533e85354810b5951c070df8e550b9
Jon Carlson <jdcarlson@gmail.com>
2018-01-18 12:10:44 -0500

    Button types and states

--- packages/micro-ui-button/src/micro-ui-button.js
+++ packages/micro-ui-button/src/micro-ui-button.js
@@ -2,12 +2,15 @@ import Component from 'inferno-component'
 import { MDCRipple } from '@material/ripple'
 
 const BASE_CLASS = 'mdc-button'
-const RAISED_CLASS = 'mdc-button--raised'
-const UNELEVATED_CLASS = 'mdc-button--unelevated'
-const STROKED_CLASS = 'mdc-button--stroked'
-const DENSE_CLASS = 'mdc-button--dense'
-const COMPACT_CLASS = 'mdc-button--compact'
-const ICON_ONLY_CLASS = 'mdc-button--icon-only'
+const STATE_INFO = 'mdc-button-info'
+const STATE_SUCCESS = 'mdc-button-success'
+const STATE_DANGER = 'mdc-button-danger'
+const TYPE_PRIMARY = 'mdc-button-primary'
+const TYPE_SECONDARY = 'mdc-button-secondary'
+const TYPE_TERTIARY = 'mdc-button-tertiary'
+const MODIFIER_INVERSE = 'mdc-button--inverse'
+const MODIFIER_ICON_ONLY = 'mdc-button--icon-only'
+const MODIFIER_ELEVATED = 'mdc-button--raised'
 
 export class MCButton extends Component {
 
@@ -24,8 +27,8 @@ export class MCButton extends Component {
 
         return (
             <button
-                className={this.getClassNames()}
-                disabled={this.props.disabled}
+                className={ this.getClassNames() }
+                disabled={ this.props.disabled }
                 ref={ element => this.element = element }>
 
                 { this.props.icon &&
@@ -56,12 +59,15 @@ export class MCButton extends Component {
         let classNames = [BASE_CLASS]
         let iconOnly = !this.props.text && (this.props.icon || this.props.leftIcon || this.props.rightIcon)
 
-        if (iconOnly) classNames.push(ICON_ONLY_CLASS)
-        if (this.props.raised) classNames.push(RAISED_CLASS)
-        if (this.props.unelevated) classNames.push(UNELEVATED_CLASS)
-        if (this.props.stroked) classNames.push(STROKED_CLASS)
-        if (this.props.dense) classNames.push(DENSE_CLASS)
-        if (this.props.compact) classNames.push(COMPACT_CLASS)
+        if (iconOnly) classNames.push(MODIFIER_ICON_ONLY)
+        if (this.props.info) classNames.push(STATE_INFO)
+        if (this.props.success) classNames.push(STATE_SUCCESS)
+        if (this.props.danger) classNames.push(STATE_DANGER)
+        if (this.props.primary) classNames.push(TYPE_PRIMARY)
+        if (this.props.secondary) classNames.push(TYPE_SECONDARY)
+        if (this.props.tertiary) classNames.push(TYPE_TERTIARY)
+        if (this.props.elevated) classNames.push(MODIFIER_ELEVATED)
+        if (this.props.inverse) classNames.push(MODIFIER_INVERSE)
 
         return classNames.join(' ')
     }
