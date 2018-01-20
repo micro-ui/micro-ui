856e0691d3641eb3a5e22554692acbfbe0837228
Jon Carlson <jdcarlson@gmail.com>
2018-01-20 09:25:31 -0500

    Make demo page always load in development

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,45 +1,52 @@
 const webpack = require('webpack')
 const path = require('path')
 
-module.exports = {
-	entry: './demo/src/index.js',
-	output: {
-        path: __dirname + '/demo',
-		filename: 'bundle.js',
-		publicPath: 'http://localhost:8080/'
-	},
-	module: {
-		loaders: [{
-			test: /\.js$/,
-			loader: 'babel-loader'
-		},{
-            test: /\.scss$/,
-            use: ['style-loader', 'css-loader', {
-                loader: 'sass-loader',
-                options: {
-                    includePaths: [
-                        path.resolve(__dirname, './node_modules')
-                    ]
-                }
-            }],
-            issuer: /\.[tj]s$/i,
-        }]
-	},
-	devServer: {
-		contentBase: './',
-		port: 8080,
-		noInfo: false,
-		hot: true,
-		inline: true,
-		proxy: {
-			'/': {
-				bypass: function (req, res, proxyOptions) {
-					return '/demo/index.html'
+module.exports = env => {
+	return {
+		entry: './demo/src/index.js',
+		output: {
+			path: __dirname + '/demo',
+			filename: 'bundle.js',
+			publicPath: 'http://localhost:8080/'
+		},
+		module: {
+			loaders: [{
+				test: /\.js$/,
+				loader: 'babel-loader'
+			},{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', {
+					loader: 'sass-loader',
+					options: {
+						includePaths: [
+							path.resolve(__dirname, './node_modules')
+						]
+					}
+				}],
+				issuer: /\.[tj]s$/i,
+			}]
+		},
+		devServer: {
+			contentBase: './',
+			port: 8080,
+			noInfo: false,
+			hot: true,
+			inline: true,
+			proxy: {
+				'/': {
+					bypass: function (req, res, proxyOptions) {
+						return '/demo/index.html'
+					}
 				}
 			}
-		}
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin()
-	]
+		},
+		plugins: [
+			new webpack.HotModuleReplacementPlugin(),
+			new webpack.DefinePlugin({
+				'process.env': {
+					'NODE_ENV': JSON.stringify(env && env.NODE_ENV ? env.NODE_ENV : 'production')
+				}
+			}),
+		]
+	}
 }
